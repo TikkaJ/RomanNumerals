b2e4c3aab0647a040cc39d38e168d84e2d1f1c72
jatikka <Adsam1990!>
2017-10-12 23:04:26 +0300

    Assignment 5 finished.

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,62 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class RomanNumerals {
-	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
-		
+	private Map<Integer, String> conversions = new HashMap<>();
+	
+	public RomanNumerals() {
+		conversions.put(1, "I");
+		conversions.put(4, "IV");
+		conversions.put(5, "V");
+		conversions.put(9, "IX");
+		conversions.put(10, "X");
+		conversions.put(50, "L");
+		conversions.put(80, "LXXX");
+		conversions.put(90, "XC");
+		conversions.put(100, "C");
+		conversions.put(900, "CM");
+		conversions.put(1000, "M");
 	}
+	
+	public String romanNum(int convertToInteger) {
+		String romanNumeral = conversions.get(convertToInteger);
+		if(romanNumeral == null) {
+			romanNumeral = "";
+			while(convertToInteger > 0) {
+				if(convertToInteger >= 1000) {
+					romanNumeral = romanNumeral + conversions.get(1000);
+					convertToInteger = convertToInteger - 1000;
+				}else if(convertToInteger >= 900) {
+					romanNumeral = romanNumeral + conversions.get(900);
+					convertToInteger = convertToInteger - 900;
+				}else if(convertToInteger >= 100) {
+					romanNumeral = romanNumeral + conversions.get(100);
+					convertToInteger = convertToInteger - 100;
+				} else if(convertToInteger >= 90) {
+					romanNumeral = romanNumeral + conversions.get(90);
+					convertToInteger = convertToInteger - 90;
+				}else if(convertToInteger >= 80) {
+					romanNumeral = romanNumeral + conversions.get(80);
+					convertToInteger = convertToInteger - 80; 
+				}else if(convertToInteger >= 10) {
+					romanNumeral = romanNumeral + conversions.get(10);
+					convertToInteger = convertToInteger - 10;
+				} else if(convertToInteger >= 9) {
+					romanNumeral = romanNumeral + conversions.get(9);
+					convertToInteger = convertToInteger - 9;
+				} else if(convertToInteger >= 5) {
+					romanNumeral = romanNumeral + conversions.get(5);
+					convertToInteger = convertToInteger - 5;
+				} else if(convertToInteger >= 4) {
+					romanNumeral = romanNumeral + conversions.get(4);
+					convertToInteger = convertToInteger - 4;
+				} else {
+					romanNumeral = romanNumeral + conversions.get(1);
+				     convertToInteger = convertToInteger - 1;
+				}
+			}
+		}
+		return romanNumeral;
+	}
+	
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -3,10 +3,68 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestRomanNumerals {
+	private RomanNumerals converts = new RomanNumerals();
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void romanNumber() {
+		assertArabic(1, "I");
+	}
+	@Test
+	public void romanNumber2() {
+		assertArabic(2, "II");
+	}
+	@Test
+	public void romanNumber5() {
+		assertArabic(5, "V");
+	}
+	@Test
+	public void romanNumber10() {
+		assertArabic(10, "X");
+	}
+	@Test
+	public void romanNumber50() {
+		assertArabic(50, "L");
+	}
+	@Test
+	public void romanNumber100() {
+		assertArabic(100, "C");
+	}
+	@Test
+	public void romanNumber1000() {
+		assertArabic(1000, "M");
+	}
+	@Test
+	public void romanNumber11() {
+		assertArabic(11, "XI");
+	}
+	@Test
+	public void romanNumber90() {
+		assertArabic(90, "XC");
+	}
+	@Test
+	public void romanNumber99() {
+		assertArabic(99, "XCIX");
+	}
+	@Test
+	public void romanNumber1199() {
+		assertArabic(1199, "MCXCIX");
+	}
+	@Test
+	public void romanNumber1984() {
+		assertArabic(1984, "MCMLXXXIV");
+	}
+	@Test
+	public void romanNumber2014() {
+		assertArabic(2014, "MMXIV");
+	}	
+				
+	@Test
+	public void romanNumber3999() {
+		assertArabic(3999, "MMMCMXCIX");
+	}
+	
+	private void assertArabic(int arabic, String romNum) {
+		assertEquals(romNum, converts.romanNum(arabic));
 	}
 
 }
